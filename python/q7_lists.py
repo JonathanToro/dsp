f4cff7cb9001d77249b463689a90e73caa07eda0
JonathanToro <jtoro1118@gmail.com>
2016-08-30 23:17:18 -0400

    Update q7_lists.py

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -97,6 +97,7 @@ def remove_adjacent(nums):
 
 
 def linear_merge(list1, list2):
+	return sorted(list1+list2)
     """
     Given two lists sorted in increasing order, create and return a
     merged list of all the elements in sorted order. You may modify
